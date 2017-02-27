32abfff3abe83ab26fb8864104331d4ba8b40c8f
RyanQu <ryanqu94@sina.com>
2017-02-27 15:10:20 -0500

    mp3 to wav

---
+++ mp32wav.py
@@ -0,0 +1,49 @@
+import subprocess
+import wave
+import struct
+import numpy
+import csv
+import sys
+import os
+from pydub import *
+ 
+def read_wav(wav_file):
+    """Returns two chunks of sound data from wave file."""
+    w = wave.open(wav_file)
+    n = 60 * 10000
+    if w.getnframes() < n * 2:
+        raise ValueError('Wave file too short')
+    frames = w.readframes(n)
+    wav_data1 = struct.unpack('%dh' % n, frames)
+    frames = w.readframes(n)
+    wav_data2 = struct.unpack('%dh' % n, frames)
+    return wav_data1, wav_data2
+ 
+def compute_chunk_features(mp3_file):
+    """Return feature vectors for two chunks of an MP3 file."""
+    # Extract MP3 file to a mono, 10kHz WAV file
+    song = AudioSegment.from_mp3(mp3_file)
+    db = song.dBFS
+    song.export('temp.wav', format="wav")
+
+    # We'll cover how the features are computed in the next section!
+    return db
+ 
+# Main script starts here
+# =======================
+ 
+for path, dirs, files in os.walk('/Users/RyanQu/Documents/Workspace/Git/Music_Generator/mp3/'):
+    for f in files:
+        if not f.endswith('.mp3'):
+            # Skip any non-MP3 files
+            continue
+        mp3_file = os.path.join(path, f)
+        # Extract the track name (i.e. the file name) plus the names
+        # of the two preceding directories. This will be useful
+        # later for plotting.
+        tail, track = os.path.split(mp3_file)
+        
+        try:
+            db = compute_chunk_features(mp3_file)
+        except:
+            continue
