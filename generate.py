95893394d37dd1bd58e5af8c5d6ef49f166b0f8a
RyanQu <ryanqu94@sina.com>
2017-02-27 00:44:40 -0500

    tune the graph

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import numpy
 import scipy.io.wavfile
 import matplotlib.pyplot
 
-doremi = [523.0, 587.0, 659.0, 698.0, 784.0] # C,D,E,F,G or Do,Re,Mi,Fa,So
+doremi = [523.0, 587.0, 659.0, 698.0, 784.0, 1035.0, 1200.0] # C,D,E,F,G or Do,Re,Mi,Fa,So
 NFFT = 1024       # the length of the windowing segments
 dt = 0.0005
 Fs = int(1.0/dt)  # the sampling frequency
@@ -22,7 +22,6 @@ print song
 print type(song)
 #print type(song.tolist())
 
-
 scipy.io.wavfile.write('doremi.wav', sampling_rate, song.astype(numpy.int16))
-matplotlib.pyplot.specgram(song, NFFT=NFFT, Fs=Fs, noverlap=500) # generate spectogram
+matplotlib.pyplot.specgram(song, NFFT=NFFT, Fs=Fs, noverlap=900) # generate spectogram
 matplotlib.pyplot.savefig('doremi.png')
