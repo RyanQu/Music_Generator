1e426fcd3a6bd405464efdc566e7589692cab422
RyanQu <ryanqu94@sina.com>
2017-02-27 15:43:07 -0500

    generate .wav file

--- readwav.py
+++ readwav.py
@@ -11,13 +11,13 @@ def read_wav(wav_file):
     """Returns two chunks of sound data from wave file."""
     w = wave.open(wav_file)
     n = 60 * 10000
-    if w.getnframes() < n * 2:
-        raise ValueError('Wave file too short')
+    # if w.getnframes() < n * 2:
+    #     raise ValueError('Wave file too short')
     frames = w.readframes(n)
-    wav_data1 = struct.unpack('%dh' % n, frames)
-    frames = w.readframes(n)
-    wav_data2 = struct.unpack('%dh' % n, frames)
-    return wav_data1, wav_data2
+    print type(frames)
+    wav_data = struct.unpack('%dh' % n, frames)
+    print type(wav_data)
+    return wav_data
 
 for path, dirs, files in os.walk('/Users/RyanQu/Documents/Workspace/Git/Music_Generator/wav/'):
     for f in files:
@@ -25,12 +25,13 @@ for path, dirs, files in os.walk('/Users/RyanQu/Documents/Workspace/Git/Music_Ge
             # Skip any non-MP3 files
             continue
         wav_file = os.path.join(path, f)
+        print wav_file
         # Extract the track name (i.e. the file name) plus the names
         # of the two preceding directories. This will be useful
         # later for plotting.
         tail, track = os.path.split(wav_file)
         
         try:
-            db = compute_chunk_features(wav_file)
+            wav_data = read_wav(wav_file)
         except:
             continue
