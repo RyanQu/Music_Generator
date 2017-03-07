c9d8952075367a6b587bac091826fb85a00cd72f
RyanQu <ryanqu94@sina.com>
2017-03-06 22:56:50 -0500

    change function structure

--- readwav.py
+++ readwav.py
@@ -6,7 +6,14 @@ import numpy as np
 import pylab as pl
 import csv
 import os
-import matplotlib.pyplot
+
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import maximum_filter
+from scipy.ndimage.morphology import (generate_binary_structure,
+                                      iterate_structure, binary_erosion)
+import hashlib
+from operator import itemgetter
 from pydub import *
 
 NFFT = 1024       # the length of the windowing segments
@@ -30,29 +37,36 @@ def read_wav(wav_file):
     wav_data = np.fromstring(str_data, dtype=np.short)
     wav_data.shape = -1, 2
     wav_data = wav_data.T
+
+    return wav_data, n
+
+def spectrum(wav_data, plot=False, n):
+
     time = np.arange(0, n) * (1.0 / framerate)
 
-    pl.figure()
-    pl.subplot(211)
-    pl.plot(time, wav_data[0])
-    pl.subplot(212)
-    pl.plot(time, wav_data[1], c="g")
-    pl.xlabel("time (seconds)")
-    pl.savefig('%s_pylab.png' %wav_file)
+    if plot:
+        pl.figure()
+        pl.subplot(211)
+        pl.plot(time, wav_data[0])
+        pl.subplot(212)
+        pl.plot(time, wav_data[1], c="g")
+        pl.xlabel("time (seconds)")
+        pl.savefig('%s_pylab.png' %wav_file)
 
-    matplotlib.pyplot.figure()
-    matplotlib.pyplot.specgram(wav_data[0], NFFT=NFFT, Fs=Fs, noverlap=900) # generate spectogram
-    matplotlib.pyplot.savefig('%s_1.png' %wav_file)
+    plt.figure()
+    arr2D = plt.specgram(wav_data[0], NFFT=NFFT, Fs=Fs, noverlap=900)[0] # generate spectogram
+    plt.savefig('%s_1.png' %wav_file)
 
-    matplotlib.pyplot.figure()
-    matplotlib.pyplot.specgram(wav_data[1], NFFT=NFFT, Fs=Fs, noverlap=900) # generate spectogram
-    matplotlib.pyplot.savefig('%s_2.png' %wav_file)
+    # plt.figure()
+    # plt.specgram(wav_data[1], NFFT=NFFT, Fs=Fs, noverlap=900)[0] # generate spectogram
+    # plt.savefig('%s_2.png' %wav_file)
+    return arr2D
 
-    return wav_data
+
 
 for path, dirs, files in os.walk('/Users/RyanQu/Documents/Workspace/Git/Music_Generator/wav/'):
     for f in files:
-        if not f.endswith('.wav'):
+        if not f.endswith('temp.wav'):
             # Skip any non-MP3 files
             continue
 
